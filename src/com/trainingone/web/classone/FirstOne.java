4dc8465346f51fe9c6775fdd9929ca9534f60e81
Ravikumar <ravi.seedarla@gmail.com>
2017-12-19 23:06:13 -0500

    first Commit

---
+++ src/com/trainingone/web/classone/FirstOne.java
@@ -0,0 +1,40 @@
+// Package - ONE
+package com.trainingone.web.classone;
+// Class - ONE
+public class FirstOne {
+	// Method () - Multiple
+	public static void main(String[] args) {
+		// variable , is a value to story any type of variable type.
+		// variable , should be declared only ONCE , but can define any number of times .
+		// variable , name should be UNIQUE
+		
+		// int=type of the variable(what you want to store.) , intVariable=name of the variable
+		int intVariable; // Only declaring varaible // integer type variable, can store only numbers.
+		String stringVariable = "Ravi Kumar" ; // string type variable, can store only string
+		boolean booleanVariable = true; // boolean type variable, can story true or false
+		
+		intVariable = 10; // defining variable
+		
+		// if - okavela // else - lekapothe
+		// if statement validates and accepts  ONLY true/false condition.
+		
+		booleanVariable = false; // changing variable value between the code
+		
+		if(booleanVariable){
+			
+			System.out.println(" condition is TRUE, so inside IF statement ");
+			System.out.println(" TRUE TRUE TRUE ");
+			
+		}else{
+			
+			System.out.println(" condition is FALSE, so inside ELSE statement ");
+			System.out.println(" FALSE FALSE FALSE ");
+		}
+	}
+	
+	// Method ()
+	public void Ravi(){
+		
+	}
+
+}
---
+++ src/com/trainingone/web/classone/FirstTwo.java
@@ -0,0 +1,7 @@
+package com.trainingone.web.classone;
+
+public class FirstTwo {
+
+	
+
+}
